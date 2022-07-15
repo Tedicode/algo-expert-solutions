da26fdadc6291403dea5e8cc7c5cf49fa082725a
Tedi Lowney <tmlowney1@gmail.com>
2022-07-15 12:30:27 -0400

    Big o for naive solution arrayOfProducts

--- array-of-products-naive.js
+++ array-of-products-naive.js
@@ -1,6 +1,11 @@
 function arrayOfProducts(array) {
   // Write your code here.
 
+  // Time: O(n^2) - where n is length of original input array
+  // nested for loop iterating n times (also using reduce inside the for loop).
+
+  // Space: O(n) - where n is length of original input array
+  // storing array(s) the same length as the input array
   // loop through input array
   // for each iteration
   // build up another array with all elements except the current one

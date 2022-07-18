df63e266ef5c14e7a53040d6b90ea76b4e597d1d
Tedi Lowney <tmlowney1@gmail.com>
2022-07-18 15:00:47 -0400

    removed unneccesary boolean parameter

--- min-height-BST.js
+++ min-height-BST.js
@@ -1,4 +1,4 @@
-function minHeightBst(array, isRoot = true, root = null) {
+function minHeightBst(array, root = null) {
   // Write your code here.
 
   // take in the array
@@ -6,22 +6,22 @@ function minHeightBst(array, isRoot = true, root = null) {
   // that's the node's value
   // slice array from zero to the midpoint - thats the left array
   // slice array from midpoint to the end - thats the right array
-  // recurse on left and right, using these arrays,
-  // to insert each value
-  // using this order ensures minimum height bst
+  // recurse on left and right, using these arrays, and assigning their roots
+  // to the left child and right child
 
   if (array.length === 0) return;
 
+  // IF GETTING ERROR, THEN MAY BE OFF BY ONE ERROR? SO MAYBE midpoint be array.length/2 - 1
   let midpoint = Math.floor(array.length / 2);
   let value = array[midpoint];
-  if (isRoot) root = new BST(value);
+  if (!root) root = new BST(value);
   else root.insert(value);
 
   let leftArr = array.slice(0, midpoint);
   let rightArr = array.slice(midpoint + 1);
 
-  minHeightBst(leftArr, false, root);
-  minHeightBst(rightArr, false, root);
+  minHeightBst(leftArr, root);
+  minHeightBst(rightArr, root);
 
   // each function call returns the root
   return root;
@@ -31,7 +31,7 @@ function minHeightBst(array, isRoot = true, root = null) {
 // because we recurse, creating as many calls as we have elements in in array (n)
 // and at each visit we are performing the .insert operation,
 // and .insert is a BST Class method with a time complexity itself of O(n)
-// in the worst case and O(logn) in average case
+// in the worst case and logn in average case
 // - but we are ensuring an average case by using .insert from the beginning
 // (to create MINiMUM HEIGHT BST)
 // --- so overall complexity is n * log n -->  O(nlogn)
@@ -41,8 +41,8 @@ function minHeightBst(array, isRoot = true, root = null) {
 // we will use up frames on the call stack with each recursive call
 // with each recursive call, we are processing half as many elements
 // so we will have 'log n' calls on the stack at most at one time...
-// BUT we are creating and storing a BST of n nodes,
-// --> so complexity becomes O(n)
+// BUT we are creating a BST of n nodes,
+//  so complexity becomes O(n)
 
 class BST {
   constructor(value) {

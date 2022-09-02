c2aed9e153175272f4a98f317cfff40243a8bb3e
Tedi Lowney <tmlowney1@gmail.com>
2022-09-02 11:38:13 -0400

    structy - LinkedList - sum vals - recursive

--- linkedList-sumNodes-structy.js
+++ linkedList-sumNodes-structy.js
@@ -8,15 +8,26 @@
 const sumList = (head) => {
   // todo
 
+  // recursive approach :
+
+  let nodeSum = 0;
+  function helperRecurse(sum, node) {
+    if (node === null) return sum;
+    sum += node.val;
+    return helperRecurse(sum, node.next);
+  }
+
+  return helperRecurse(nodeSum, head);
+
   // iterative:
 
-  let sum = 0;
-  let current = head;
-  while (current !== null) {
-    sum += current.val;
-    current = current.next;
-  }
-  return sum;
+  // let sum = 0;
+  // let current = head;
+  // while (current !== null) {
+  //   sum += current.val;
+  //   current = current.next;
+  // }
+  // return sum;
 };
 
 module.exports = {

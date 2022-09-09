70d600eb8d7f1a6b6f38998a082ba94c4419ca92
Tedi Lowney <tmlowney1@gmail.com>
2022-09-08 20:41:27 -0400

    linked list values - recursive - structy

--- linkedList-values-structy.js
+++ linkedList-values-structy.js
@@ -6,19 +6,31 @@
 // }
 
 const linkedListValues = (head) => {
-  // iterative:
+  // // iterative:
 
-  let arr = [];
+  // let arr = []
 
-  let current = head;
-  while (current !== null) {
-    arr.push(current.val);
-    current = current.next;
-  }
+  // let current = head
+  // while(current !== null) {
+  //   arr.push(current.val)
+  //   current = current.next
+  // }
 
-  return arr;
+  // return arr
   // Time: O(N)
   // Space: O(N)
+
+  // recursive:
+
+  //   let arrayOfVals = []
+
+  function helperRecurse(arr, node) {
+    if (node === null) return arr;
+    arr.push(node.val);
+    return helperRecurse(arr, node.next);
+  }
+
+  return helperRecurse(arrayOfVals, head);
 };
 
 module.exports = {

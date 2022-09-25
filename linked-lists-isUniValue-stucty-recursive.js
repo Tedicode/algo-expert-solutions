58318c16be931f60c5dec5c5f0ff33ee900f012e
Tedi Lowney <tmlowney1@gmail.com>
2022-09-24 22:16:04 -0400

    Big O for recursive solution linked-lists-is-uniValue

--- linked-lists-isUniValue-stucty-recursive.js
+++ linked-lists-isUniValue-stucty-recursive.js
@@ -8,6 +8,12 @@
 const isUnivalueList = (head) => {
   // todo
 
+  // recursive:
+  // Time: O(n)
+  // will have as many iterations as we have nodes on the LL b/c we'll evaluate each node. And each iteration only comprises constant time operations
+  // Space: O(n)
+  // will have as many frames on the callstack as we have nodes on the LL
+
   // traverse the entire list
   // return false as soon as you encounter a different value
   // else, if you arrive at last node (null), then return true

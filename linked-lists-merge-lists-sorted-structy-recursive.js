53b7a681d5b6e07611da12e44561fa41b73b1707
Tedi Lowney <tmlowney1@gmail.com>
2022-09-24 21:43:48 -0400

    Big O of merge linked lists sorted recursive solution

--- linked-lists-merge-lists-sorted-structy-recursive.js
+++ linked-lists-merge-lists-sorted-structy-recursive.js
@@ -1,5 +1,9 @@
 // recursive approach:
 
+// Time: O(n+m) with n as # of nodes in LL1 and m is # nodes in LL2
+// Space: O(n+m) - in worse case scenario (LL1 length == LL2 length),
+// we'll have as many iterations as we have total nodes in LL1 + LL2
+
 if (head1 === null && head2 === null) return null;
 if (head1 === null) return head2;
 if (head2 === null) return head1;

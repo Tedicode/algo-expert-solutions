34ce17928287001e10e0eec1ba0013adaada8671
Tedi Lowney <tmlowney1@gmail.com>
2022-10-08 21:05:42 -0400

    Big O Analysis - tree BFS - structy

--- tree-breadth-first-values.js
+++ tree-breadth-first-values.js
@@ -22,6 +22,17 @@ const breadthFirstValues = (root) => {
 
   return values;
 };
+// time: O(n^2) - while loop iterates n times, and will execute the .shift() method
+// each time, which itself has time complexity of n
+// * more optimal would be implementing our own class for a queue, using a linkedList.
+// that way, we could make accessible methods that can both enqueue and dequeue in constant time, giving the algorithm an overall time complexity of O(n)
+
+// space: O(n) - we have an array for values and an array for queue, each of length n
+// O(2n) space that reduces to O(n)
+
+// ***A recursive solution:
+// is not practical for breath first search, because it needs a queue order
+// so always use an iterative solution for BFS
 
 module.exports = {
   breadthFirstValues,

bc1e67b7e71744d3cc5201c94d23b7b411fbd9c9
Tedi Lowney <tmlowney1@gmail.com>
2022-10-11 17:36:29 -0400

    implement hashTable class - method: hash function

---
+++ hash-table-class-implement.js
@@ -0,0 +1,17 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    const WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+
+    return total;
+  }
+}

7064cdc99b1bcda3fe57a24d363a3443235c1dc0
Tedi Lowney <tmlowney1@gmail.com>
2022-10-11 17:48:22 -0400

    implement hashTable class: methods get and set

--- hash-table-class-implement.js
+++ hash-table-class-implement.js
@@ -14,4 +14,32 @@ class HashTable {
 
     return total;
   }
+
+  set(key, val) {
+    let keyIdx = this._hash(key);
+    if (this.keyMap[keyIdx]) this.keyMap[keyIdx].push([key, val]);
+    this.keyMap[keyIdx] = [[key, val]];
+  }
+
+  get(key) {
+    let keyIdx = this._hash(key);
+    let valsArray = this.keyMap[keyIdx];
+    if (valsArray.length > 1) {
+      // then iterate through this array (of arrays) to find the array w/ this key
+      for (let [theKey, theVal] of valsArray) {
+        if (theKey === key) return theVal;
+      }
+    } else if (valsArray.length === 1) return valsArray[0][1];
+    return undefined;
+  }
 }
+
+let myHashTable = new HashTable();
+
+myHashTable.set("pink", "strawberry");
+myHashTable.set("orange", "tangerine");
+myHashTable.set("blue", "blueberry");
+
+console.log(myHashTable.get("blue"));
+console.log(myHashTable.get("pink"));
+console.log(myHashTable.get("orange"));

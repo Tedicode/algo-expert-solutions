2b71f02f66f3bf7e30e69a93d70eb4f3cca1629a
Tedi Lowney <tmlowney1@gmail.com>
2022-10-17 14:41:33 -0400

    leaf-list trees structy

---
+++ leaf-list-trees-structy.js
@@ -0,0 +1,28 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+const leafList = (root) => {
+  let leavesArr = [];
+
+  function helperRecurse(node) {
+    if (!node) return;
+    if (node.left === null && node.right === null)
+      return leavesArr.push(node.val);
+
+    if (node.left) helperRecurse(node.left);
+    if (node.right) helperRecurse(node.right);
+  }
+
+  helperRecurse(root);
+
+  return leavesArr;
+};
+
+module.exports = {
+  leafList,
+};

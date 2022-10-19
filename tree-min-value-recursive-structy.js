f2cf79759e0fa8cd225f03802ad494b8e30c79fb
Tedi Lowney <tmlowney1@gmail.com>
2022-10-18 21:36:21 -0400

    Big O analysis: tree-min-value recursive - structy

--- tree-min-value-recursive-structy.js
+++ tree-min-value-recursive-structy.js
@@ -13,6 +13,13 @@ const treeMinValue = (root, min = Infinity) => {
   if (root.val < min) min = root.val;
   return Math.min(treeMinValue(root.left, min), treeMinValue(root.right, min));
 
+  // Time: O(n) - where n is num of nodes on input tree. We visit each nodes
+  // each visit, we're processing the node with constant time operations
+
+  // Space: O(n) worst case, O(logn) average case
+  // - not using any aux storage but will use up to n frames on the call stack
+  // - though on average just log n frames on the stack
+
   // function helperRecurse(node) {
   //   if (node === null) return;
   //   if (node.val < min) min = node.val;

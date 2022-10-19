e531f144b96f7a4e08f727db77c591ac68d60b35
Tedi Lowney <tmlowney1@gmail.com>
2022-10-18 21:33:21 -0400

    tree-min-value iterative Depth first structy

--- tree-min-value-iterative-structy.js
+++ tree-min-value-iterative-structy.js
@@ -10,14 +10,27 @@ const treeMinValue = (root) => {
   // In Breadth first (using a queue),  the "next up" to process will be all siblings before children
   // In Depth first (using a stack), "next up" to process will be children before siblings
 
-  // iterative - BFS
+  // // iterative - BFS
+  // let min = Infinity;
+  // let queue = [root];
+  // while (queue.length) {
+  //   let current = queue.shift();
+  //   if (current.val < min) min = current.val;
+  //   if (current.left) queue.push(current.left);
+  //   if (current.right) queue.push(current.right);
+  // }
+
+  // return min;
+
+  // iterative - DFS;
   let min = Infinity;
-  let queue = [root];
-  while (queue.length) {
-    let current = queue.shift();
+  let stack = [root];
+
+  while (stack.length) {
+    let current = stack.pop();
     if (current.val < min) min = current.val;
-    if (current.left) queue.push(current.left);
-    if (current.right) queue.push(current.right);
+    if (current.left) stack.push(current.left);
+    if (current.right) stack.push(current.right);
   }
 
   return min;

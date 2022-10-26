06754ae10a304379f08354a9ccff50fe0b2cdb23
Tedi Lowney <tmlowney1@gmail.com>
2022-10-26 12:20:20 -0400

    implement graph class

---
+++ implement-graph-class.js
@@ -0,0 +1,21 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) this.adjacencyList[vertex1] = [];
+    if (!this.adjacencyList[vertex2]) this.adjacencyList[vertex2] = [];
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((el) => el !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((el) => el !== v1);
+  }
+}

9598778a99e0f7f6f31a77790de1675ff81f8196
Tedi Lowney <tmlowney1@gmail.com>
2022-10-27 15:23:48 -0400

    Big O analysis of BreathFirst solution : has-path graph structy

--- has-path-graph-breadthFirst-structy.js
+++ has-path-graph-breadthFirst-structy.js
@@ -1,6 +1,13 @@
 const hasPath = (graph, src, dst) => {
   // breath first traversal
 
+  // Time: O(e) - where e is number of edges in the graph
+  // -- though the while loop, containin a shift() operation might converge toward
+  // n^2 in the worst case (where graph is basically a line), this complexity is surpassed
+  // by the operation of processing every single neighbor of every single vertex (processing each edge)
+  // Space: O(n) - where n is number of nodes/vertices in the graph
+  // -- b/c we store a queue which may grow, in worst case scenario, to a length of total number of nodes/vertices
+
   let queue = [src];
 
   while (queue.length) {

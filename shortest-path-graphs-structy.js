f843fd119eb67083a39b1dbed609aa0f1b559f06
Tedi Lowney <tmlowney1@gmail.com>
2022-10-30 12:04:51 -0400

    notes on shortest-path graph

--- shortest-path-graphs-structy.js
+++ shortest-path-graphs-structy.js
@@ -1,4 +1,7 @@
 const shortestPath = (edges, nodeA, nodeB) => {
+  // for most graph problems that ask you to find a shortest path,
+  // you'll want to employ breadth first search (bc you will encounter
+  // the target sooner than you would by pursuing paths deeply)
   let graph = makeGraph(edges);
   let visited = new Set();
   let queue = [[nodeA, 0]];
@@ -9,8 +12,8 @@ const shortestPath = (edges, nodeA, nodeB) => {
     if (currentNode === nodeB) return currentLevel;
 
     if (!visited.has(currentNode)) {
-      // important to mark the nodes visited here, instead of when dequeueing, b/c
-      // If you enqueue a node and don't mark it as discovered, you run the risk of enqueueing it again
+      // important to mark the nodes visited when enqueueing, instead of when dequeueing, b/c
+      // If you enqueue a node and don't mark it as discovered, you run the risk of enqueueing it again and processing duplicates
       visited.add(currentNode);
       for (let neighbor of graph[currentNode])
         queue.push([neighbor, currentLevel + 1]);
